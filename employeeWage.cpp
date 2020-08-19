79da7ac42d591e1db993c292e687a79e8411081c
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-19 20:18:07 +0530

    Added: Code to calculate part time wages

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 int main (){
              cout << "\n\tWelcome To Employee Wage Program.\n" << endl;
-    const int EMP_RATE_PER_HOUR = 20;
+    const int EMP_RATE_PER_HOUR = 20, FULL_TIME = 0, PART_TIME = 1;
     int empHrs, empWage, isPresent = 1;
-
     srand(time(0));
-    int empCheck = rand() % 2;
-    if (empCheck == isPresent)
-        empHrs = 8;
-    else
-        empHrs = 0;
+    
+    int empCheck = rand() % 3;
+    switch(empCheck){
+        case FULL_TIME:
+            empHrs = 8;
+            break;
+        case PART_TIME:
+            empHrs = 4;
+            break;
+        default:
+            empHrs = 0;
+    }
     empWage = empHrs * EMP_RATE_PER_HOUR;
     cout << "Employee Wage Per Day = " << empWage << endl;
 }
