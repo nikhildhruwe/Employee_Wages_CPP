83353c6da1d2e79de2aac9474b795bd11fffe1d0
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-20 00:12:09 +0530

    Added: Code to save employee wage into a file

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include <ctime>
 using namespace std;
+
+void writeToFile (string fileName, int empWage){
+    fstream fileStream;
+    fileStream.open(fileName, ios::out | ios::trunc);
+    if(fileStream.is_open()){
+        fileStream << empWage << endl;
+        fileStream.close();
+    }
+}
 
 int main (){
              cout << "\n\tWelcome To Employee Wage Program.\n" << endl;
     const int EMP_RATE_PER_HOUR = 20, FULL_TIME = 0, PART_TIME = 1, NUM_OF_WORKING_DAYS = 20;
     const int MAX_MONTHLY_HRS = 100;
     int empHrs, empWage, isPresent = 1, day = 0, totalEmpHrs = 0 ;
+    string fileName = "employeeWageDetails.csv";
     srand(time(0));
 
   while ( day <= NUM_OF_WORKING_DAYS && totalEmpHrs <= MAX_MONTHLY_HRS){  
@@ -25,5 +36,6 @@ int main (){
     totalEmpHrs += empHrs;
   }
     empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+    writeToFile(fileName, empWage);
     cout << "Employee Wage Per Day = " << empWage << endl;
 }
