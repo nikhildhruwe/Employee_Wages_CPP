76cdc4672cec8f50e4831b17d82e4f3510f8c1f8
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-19 20:04:00 +0530

    Added: Code to check employee's attendance

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main (){
-    cout << "Welcome To Employee Wage Program." << endl;
+             cout << "\n\tWelcome To Employee Wage Program.\n" << endl;
+    int isPresent = 1;
+    srand(time(0));
+    int empCheck = rand() % 2;
+    if (empCheck == 1)
+        cout << "Employee is Present" << endl;
+    else
+        cout << "Employee is Absent" << endl;
 }
